ff7cb634892413375fcfc613aeb60fa066484650
raykin <raykincoldxiao@gmail.com>
2011-03-04 14:30:51 +0800

    refactor code and add more doc for acts_as_dictionary

--- lib/rails_dictionary/acts_as_dictionary.rb
+++ lib/rails_dictionary/acts_as_dictionary.rb
@@ -4,31 +4,33 @@ module ActsAsDictionary
   end
 
   module ClassMethods
+
     def acts_as_dictionary
-      self.class_eval do
-        include InstanceMethods
-        belongs_to :dict_type
-        after_save :delete_dicts_cache
-        after_destroy :delete_dicts_cache
-
-        # TODO: need to add more function
-        def self.method_missing(method_id,options={},&block)
-          method_name=method_id.to_s.downcase
-          if DictType.all_types.include? method_id.to_s
-            Rails.cache.fetch("Dictionary.#{method_name}") { Dictionary.joins(:dict_type).where('dict_types.name'=>method_name).all }
-            listed_attr=Rails.cache.read("Dictionary.#{method_name}").dup
-            if options.keys.include? :locale or options.keys.include? "locale"
-              locale="name_#{options[:locale]}"
-              listed_attr.map! { |a| [a.send(locale),a.id] }
-              listed_attr.sort {|a,b| a.last <=> b.last } # maybe remove this one
-            else
-              listed_attr
-            end
+      belongs_to :dict_type
+      after_save :delete_dicts_cache
+      after_destroy :delete_dicts_cache
+      scope :dict_type_name_eq,lambda {|method_name| joins(:dict_type).where("dict_types.name" => method_name).all}
+      include InstanceMethods
+      # Generate methods like Dictionary.student_city
+      # Dictionary.student_city - a list of dictionary object which dict type is student_city
+      # Dictionary.student_city(:locale => :zh) - a select format array which can be used
+      # in view method select as choice params
+      # Is this design good?
+      def self.method_missing(method_id,options={},&block)
+        method_name=method_id.to_s.downcase
+        if DictType.all_types.include? method_id.to_s
+          Rails.cache.fetch("Dictionary.#{method_name}") { Dictionary.dict_type_name_eq(method_name) }
+          listed_attr=Rails.cache.read("Dictionary.#{method_name}").dup
+          if options.keys.include? :locale or options.keys.include? "locale"
+            locale="name_#{ options[:locale] }"
+            listed_attr.map! { |a| [a.send(locale),a.id] }
+            listed_attr.sort {|a,b| a.last <=> b.last } # maybe remove this line
           else
-            super
+            listed_attr
           end
+        else
+          super
         end
-
       end
 
     end
