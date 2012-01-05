dcfbea59f83b3d0acd5842287806c08d6d6d552f
raykin <raykincoldxiao@gmail.com>
2012-01-05 14:40:37 +0800

    remove engine. Several month ago I add engin in this gem and prepare add admin page to edit dictionary and dict_type. And now I think it would better to use other gems like rails_admin to do this work.

--- lib/rails_dictionary.rb
+++ lib/rails_dictionary.rb
@@ -1,2 +1,9 @@
-require File.join(File.dirname(__FILE__), 'rails_dictionary/railtie')
-require File.join(File.dirname(__FILE__), 'rails_dictionary/engine')
+require 'iconv'
+
+require File.join(File.dirname(__FILE__), "rails_dictionary/array_core_ext")
+require File.join(File.dirname(__FILE__), "rails_dictionary/models/active_record_extension")
+require File.join(File.dirname(__FILE__), "rails_dictionary/models/acts_as_dict_type")
+require File.join(File.dirname(__FILE__), "rails_dictionary/models/acts_as_dictionary")
+require File.join(File.dirname(__FILE__), "rails_dictionary/models/acts_as_dict_slave")
+
+::ActiveRecord::Base.send :include, RailsDictionary::ActiveRecordExtension
--- lib/rails_dictionary/engine.rb
+++
@@ -1,4 +0,0 @@
-module RailsDictionary
-  class Engine < ::Rails::Engine
-  end
-end
