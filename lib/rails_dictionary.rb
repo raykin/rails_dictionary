e53c9ec39014c423123487bf379a8fe53a7dbfec
raykin <raykincoldxiao@gmail.com>
2013-07-06 13:26:26 +0800

    fix typo

--- lib/rails_dictionary.rb
+++ lib/rails_dictionary.rb
@@ -4,7 +4,7 @@ require File.join(File.dirname(__FILE__), "rails_dictionary/array_core_ext")
 require File.join(File.dirname(__FILE__), "rails_dictionary/models/active_record_extension")
 require File.join(File.dirname(__FILE__), "rails_dictionary/models/acts_as_dict_type")
 require File.join(File.dirname(__FILE__), "rails_dictionary/models/acts_as_dictionary")
-require File.join(File.dirname(__FILE__), "rails_dictionary/models/acts_as_dict_slave"
+require File.join(File.dirname(__FILE__), "rails_dictionary/models/acts_as_dict_slave")
 
 # rake tasks not autoload in Rails4
 Dir["tasks/**/*.rake"].each { |ext| load ext } if defined?(Rake)
