3645723fc53b528124bdbe62acd0d93c03c6136a
raykin <raykincoldxiao@gmail.com>
2013-07-06 13:23:42 +0800

    make rake job available in Rails4

--- lib/rails_dictionary.rb
+++ lib/rails_dictionary.rb
@@ -4,6 +4,9 @@ require File.join(File.dirname(__FILE__), "rails_dictionary/array_core_ext")
 require File.join(File.dirname(__FILE__), "rails_dictionary/models/active_record_extension")
 require File.join(File.dirname(__FILE__), "rails_dictionary/models/acts_as_dict_type")
 require File.join(File.dirname(__FILE__), "rails_dictionary/models/acts_as_dictionary")
-require File.join(File.dirname(__FILE__), "rails_dictionary/models/acts_as_dict_slave")
+require File.join(File.dirname(__FILE__), "rails_dictionary/models/acts_as_dict_slave"
+
+# rake tasks not autoload in Rails4
+Dir["tasks/**/*.rake"].each { |ext| load ext } if defined?(Rake)
 
 ::ActiveRecord::Base.send :include, RailsDictionary::ActiveRecordExtension
