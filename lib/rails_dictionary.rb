211d64ffae52779869966a9d187dd80a0f7b4ef1
raykin <raykincoldxiao@gmail.com>
2022-04-19 13:21:16 +0800

    User on_load to reinit classes. So it should cooperate with zeitwerk lib

--- lib/rails_dictionary.rb
+++ lib/rails_dictionary.rb
@@ -1,10 +1,13 @@
-require File.join(File.dirname(__FILE__), "rails_dictionary/array_core_ext")
-require File.join(File.dirname(__FILE__), "rails_dictionary/active_record_extension")
-require File.join(File.dirname(__FILE__), "rails_dictionary/acts_as_dict_type")
-require File.join(File.dirname(__FILE__), "rails_dictionary/acts_as_dictionary")
-require File.join(File.dirname(__FILE__), "rails_dictionary/acts_as_dict_slave")
-
 # rake tasks not autoload in Rails4
 Dir[File.expand_path('../tasks/**/*.rake',__FILE__)].each { |ext| load ext } if defined?(Rake)
 
-::ActiveRecord::Base.send :include, RailsDictionary::ActiveRecordExtension
+require File.join(File.dirname(__FILE__), "rails_dictionary/array_core_ext")
+
+ActiveSupport.on_load :active_record do
+  require File.join(File.dirname(__FILE__), "rails_dictionary/active_record_extension")
+  require File.join(File.dirname(__FILE__), "rails_dictionary/acts_as_dict_type")
+  require File.join(File.dirname(__FILE__), "rails_dictionary/acts_as_dictionary")
+  require File.join(File.dirname(__FILE__), "rails_dictionary/acts_as_dict_slave")
+
+  ::ActiveRecord::Base.send :include, RailsDictionary::ActiveRecordExtension
+end
